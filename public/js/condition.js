445984b7a84edbf998ec8a9e72403c831b6e8a3f
HEECHAN PARK <bluessignal@gmail.com>
2025-09-11 17:16:05 +0900

    calculate

--- public/js/condition.js
+++ public/js/condition.js
@@ -9,16 +9,41 @@ console.log("연산자 커밋 이후");
 //점수가 60보다 크면 "노력하세요"
 //이외에는 "낙제입니다"
 function show_score() {
-let score = document.querySelector('#user_value').value;
-if (score >= 90){
-  console.log("아주 잘했습니다.")
-} else if(score >= 80){
-  console.log("잘했습니다")
-} else if(score >= 70){
-  console.log("보통입니다")
-} else if(score >= 60){
-  console.log("노력하세요")
-} else {
-  console.log("낙제입니다.")
+  let score = document.querySelector('#user_value').value;
+  if (score >= 90) {
+    console.log("아주 잘했습니다.")
+  } else if (score >= 80) {
+    console.log("잘했습니다")
+  } else if (score >= 70) {
+    console.log("보통입니다")
+  } else if (score >= 60) {
+    console.log("노력하세요")
+  } else {
+    console.log("낙제입니다.")
+  }
 }
+
+function calculate() {
+  let first = parseInt(document.querySelector('#user1').value);
+  console.log(document.querySelector('#user1').value);
+  let last = parseInt(document.querySelector('#user2').value);
+  console.log(document.querySelector('#user2').value);
+  let opr = document.querySelector('#oper').value;
+  let result = 0; // 결과값을 저장.
+  console.log(first, last, opr);
+  //조건문.
+  switch (opr) {
+    case '+':
+      result = (first) + (last); break;
+    case '-':
+      result = (first) - (last); break;
+
+    case '*':
+      result = (first) * (last); break;
+    case '/':
+      result = (first) / (last); break;
+  }
+  document.querySelector("#result").value=result;
+
+
 }
