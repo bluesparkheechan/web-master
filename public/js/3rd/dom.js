211e6e2b3eb7a4507f3f5ac52bc0657a57a3d86c
HEECHAN PARK <bluessignal@gmail.com>
2025-09-17 14:44:30 +0900

    0917

--- public/js/3rd/dom.js
+++ public/js/3rd/dom.js
@@ -41,7 +41,7 @@ function makeTr(newElement) {
     console.log(e);
     e.target.parentElement.parentElement.remove();
   });
-  btn.setAttribute('class', 'btn btn-danger'); //<button id="addBtn" Class="btn btn-danger"
+  btn.setAttribute('class', 'btn btn-danger');
   btn.innerHTML = '삭제';
   td.appendChild(btn);
   tr.appendChild(td);
@@ -50,26 +50,11 @@ function makeTr(newElement) {
 }
 
 createStdList();
+
 function createStdList() {
   for (let i = 0; i < students.length; i++) {
     let tr = makeTr(students[i]);
-    // let tr = document.createElement('tr'); //<tr></tr>
-    // //속성반복//
-    // for (let prop in students[i]) {
-    //   let td = document.createElement('td'); //<td></td>
-    //   td.innerHTML = students[i][prop]; // <td>100</td>
-    //   tr.appendChild(td); // <tr><td>100</td></tr>
-    // }
-    //삭제버튼
 
-    // //이름
-    // td = document.createElement('td'); //<td></td>
-    // td.innerHTML = students[i].stdName;
-    // tr.appendChild(td); //<tr><td>100</td></tr><tr><td>홍길동</td></tr>
-    // //점수
-    // td = document.createElement('td'); // <td></td>
-    // td.innerHTML = students[i].stdScore;
-    // tr.appendChild(td);
 
     document.querySelector('#list tbody').appendChild(tr);
 
---
+++ public/js/5th/db.js
@@ -0,0 +1,58 @@
+//db.js
+
+//이벤트.
+document.querySelector('div.container>form') // >는 바로 자식요소에서만 찾는거 
+  .addEventListener('submit', function (e) {
+    e.preventDefault(); // 기본 기능 차단
+    addPost();
+  });
+
+//글 등록
+function addPost() {
+
+  const title = document.querySelector('input[name="title"]').value;
+  const author = document.querySelector('input[name="author"]').value;
+  const xhtp = new XMLHttpRequest();
+  xhtp.open('post', 'http://localhost:3000/posts', );
+  // post 요청일 때, 요청 헤더 : 컨텐트 형식 지정.
+  xhtp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhtp.send(JSON.stringify({
+    title: title,
+    author: author
+  }));
+  xhtp.onload = function () {
+    let result = JSON.parse(xhtp.responseText);
+    let div = makeRow(result);
+    console.log(result);
+    document.querySelector('#data-container').appendChild(div);
+    //초기화
+    document.querySelector('input[name="title"]').value = '';
+    document.querySelector('input[name="author"]').value = '';
+  }
+}
+//게시글 한 건에 대한 row 생성하는 함수.
+function makeRow(post) {
+  let fields = ['id', 'title', 'author'];
+  let div = document.createElement('div');
+  for (let i = 0; i < fields.length; i++) {
+    let span = document.createElement('span');
+    span.innerHTML = post[fields[i]];
+    console.log(post[fields[i]]);
+    span.setAttribute('class', 'data-' + fields[i]);
+    div.appendChild(span);
+
+  };
+  return div; // 
+}
+
+const xhtp = new XMLHttpRequest();
+
+xhtp.open('get', 'http://localhost:3000/posts') //http://localhost:3000/posts 링크로 요청이 오면 
+xhtp.send();
+xhtp.onload = function () {
+  let data = JSON.parse(xhtp.responseText);
+  data.forEach(function (item, indx2, ary) {
+    let div = makeRow(item);
+    document.querySelector('#data-container').appendChild(div);
+  });
+}
